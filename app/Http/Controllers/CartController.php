1b3f5bcc50899a271c631d35a2a0797c5ade8ea5
Kareem <dell@pop-os.localdomain>
2024-01-03 22:52:30 +0200

    sa

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -35,22 +35,26 @@ class CartController extends BaseApiController
 
     public function remove(Request $request)
     {
-        $userId = auth()->user()?->id;
+        // $userId = auth()->user()?->id;
         // Get that latest cart for this session
         $projectId = $request->input('project_id');
-        if ($userId) {
-            $cart = Cart::where('user_id', $userId)->latest()->first();
-        } else {
-            $cart = Cart::where('session_id', $request->input('id'))->latest()->first();
-        }
+        $id = $request->input('id');
+        // if ($userId) {
+        //     $cart = Cart::where('user_id', $userId)->latest()->first();
+        // } else {
+        //     $cart = Cart::where('session_id', $request->input('id'))->latest()->first();
+        // }
+        $cart = Cart::where('user_id', $id)->orWhere('session_id', $id)->latest()->first();
+
         if ($cart) {
             $project = CartProject::where('cart_id', $cart->id)
                 ->where('project_id', $projectId)
                 ->first();
             $project?->delete();
+            $amounts = CartProject::where('cart_id', $cart->id)->sum('amount');
+            Cart::find($cart->id)->update(['total_amount' => $amounts]);
         }
-        $amounts = CartProject::where('cart_id', $cart->id)->sum('amount');
-        Cart::find($cart->id)->update(['total_amount' => $amounts]);
+
 
         return response()->json(['message' => __('cart.removed_successfully')]);
     }
