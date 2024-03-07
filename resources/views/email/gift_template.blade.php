053df77db8f97bbdbca37aa5c820aa5c9166e513
Kareem <dell@pop-os.localdomain>
2024-03-07 09:59:36 +0200

    convert mail template to inline style

--- resources/views/email/gift_template.blade.php
+++ resources/views/email/gift_template.blade.php
@@ -6,14 +6,14 @@
     <title></title>
 </head>
 <body style="margin: 0; padding: 0; font-family: Arial, sans-serif;">
-    <div style="position: relative; width: 400px; margin: 0 auto;">
-        <h4 style="display: block; position: absolute; left: 50%; transform: translateX(-50%); top: 450px;">
+    <div style="position: relative; width: 100%; max-width: 400px; margin: 0 auto;">
+        <h4 style="display: block; position: absolute; left: 50%; transform: translateX(-50%); top: 450px; margin: 0; padding: 0;">
             {{$projectName}}
         </h4>
-        <h4 style="display: block; position: absolute; left: 50%; transform: translateX(-50%); top: 310px;">
+        <h4 style="display: block; position: absolute; left: 50%; transform: translateX(-50%); top: 310px; margin: 0; padding: 0;">
             {{$recipient_name}}
         </h4>
-        <h4 style="display: block; position: absolute; left: 50%; transform: translateX(-50%); bottom: 90px;">
+        <h4 style="display: block; position: absolute; left: 50%; transform: translateX(-50%); bottom: 90px; margin: 0; padding: 0;">
             {{$sender_name}}
         </h4>
         <img src="{{$template_url}}" height="800" width="100%" style="display: block; margin: 0 auto;">
