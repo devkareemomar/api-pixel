08e982858bbadbdd12028ed50e01685df59145b5
Kareem <dell@pop-os.localdomain>
2024-03-07 09:52:50 +0200

    convert mail template to inline style

--- resources/views/email/gift_template.blade.php
+++ resources/views/email/gift_template.blade.php
@@ -1,55 +1,22 @@
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-
-    <style>
-        .position-relative {
-            position: relative;
-            width: 400px;
-        }
-
-        .cover__item {
-            position: absolute;
-            left: 50%;
-            transform: translateX(-50%);
-        }
-
-        .cover_project_name {
-            top: 450px;
-        }
-
-        .cover_sender_name {
-            top: 310px;
-        }
-
-        .cover_recipient_name {
-            bottom: 90px;
-        }
-    </style>
 </head>
-
-<body>
-    <div class="position-relative">
-        <h4 class="d-block cover__item cover_project_name">
+<body style="margin: 0; padding: 0; font-family: Arial, sans-serif;">
+    <div style="position: relative; width: 400px; margin: 0 auto;">
+        <h4 style="display: block; position: absolute; left: 50%; transform: translateX(-50%); top: 450px;">
             {{$projectName}}
         </h4>
-        <h4 class="d-block cover__item cover_sender_name">
+        <h4 style="display: block; position: absolute; left: 50%; transform: translateX(-50%); top: 310px;">
             {{$recipient_name}}
         </h4>
-
-        <h4 class="d-block cover__item cover_recipient_name">
+        <h4 style="display: block; position: absolute; left: 50%; transform: translateX(-50%); bottom: 90px;">
             {{$sender_name}}
         </h4>
-
-        <img src="{{$template_url}}" height="800" width="100%" />
+        <img src="{{$template_url}}" height="800" width="100%" style="display: block; margin: 0 auto;">
     </div>
 </body>
-
 </html>
-
-
